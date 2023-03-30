c3797b3f2a890a18472ae496ca785460593ba17c
Facundo Llampa <a.facundollampa@gmail.com>
2023-03-29 21:14:10 -0300

    clase 01

---
+++ src/utils/theme.js
@@ -0,0 +1,18 @@
+export const COLORS = {
+  primary: '#FF585D',
+  secondaty: '#F9777B',
+  text: '#263238',
+  inactive: '#A0A0A0',
+  white: '#FFFFFF',
+  grey: '#F1F1F1'
+}
+
+export const SPACING = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 40,
+  xxl: 48,
+  xxxl: 64
+}
