b4fea273f32293bc21968cd3447c9c93e1867491
Facundo Llampa <a.facundollampa@gmail.com>
2023-04-05 18:43:13 -0300

    data and assets

--- src/api/data.js
+++ src/api/data.js
@@ -6,93 +6,116 @@ export const data = [
     rating: '4.9',
     description: 'Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.',
     images: [
-      require('../../assets/images/beach_house.webp'),
-      require('../../assets/images/beach_house.webp'),
-      require('../../assets/images/beach_house.webp'),
-      require('../../assets/images/beach_house.webp'),
-      require('../../assets/images/beach_house.webp')
+      require('../../assets/images/beach_house_1.webp'),
+      require('../../assets/images/beach_house_2.webp'),
+      require('../../assets/images/beach_house_3.webp')
     ],
-    price: '$1000/noche'
+    price: '$1000/noche',
+    locationCoordinates: {
+      latitude: -24.1893858,
+      longitude: -65.2938462
+    }
   },
   {
     id: '2',
     title: 'Cabaña rústica en la montaña',
     location: 'Malibú, California',
-    rating: '4.9',
-    description: 'Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.',
+    rating: '3.5',
+    description: 'Bienvenido a nuestra acogedora cabaña en las montañas. Si estás buscando una escapada tranquila en medio de la naturaleza, ¡has encontrado el lugar perfecto! Nuestra cabaña es ideal para parejas o pequeñas familias que buscan relajarse y disfrutar de la belleza natural de la zona.',
     images: [
-      require('../../assets/images/cabin_mountain.webp'),
-      require('../../assets/images/cabin_mountain.webp'),
-      require('../../assets/images/cabin_mountain.webp')
+      require('../../assets/images/cabin_mountain_1.webp'),
+      require('../../assets/images/cabin_mountain_2.webp'),
+      require('../../assets/images/cabin_mountain_3.webp')
     ],
-    price: '$500/noche'
+    price: '$500/noche',
+    locationCoordinates: {
+      latitude: -24.1893858,
+      longitude: -65.2938462
+    }
   },
   {
     id: '3',
     title: 'Acogedor apartamento en la ciudad',
     location: 'Malibú, California',
-    rating: '4.9',
-    description: 'Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.',
+    rating: '4.2',
+    description: '¡Bienvenido a nuestro hermoso piso en el corazón de la ciudad! Si buscas un lugar cómodo y céntrico para alojarte durante tus vacaciones o viaje de negocios, has encontrado el lugar perfecto. Nuestro piso es un espacio moderno y luminoso con todas las comodidades que necesitas para disfrutar de tu estancia.',
     images: [
-      require('../../assets/images/flat_city.webp'),
-      require('../../assets/images/flat_city.webp'),
-      require('../../assets/images/flat_city.webp')
+      require('../../assets/images/flat_city_1.webp'),
+      require('../../assets/images/flat_city_2.webp'),
+      require('../../assets/images/flat_city_3.webp')
     ],
-    price: '$250/noche'
+    price: '$250/noche',
+    locationCoordinates: {
+      latitude: -24.1893858,
+      longitude: -65.2938462
+    }
   },
   {
     id: '4',
     title: 'Casa de campo junto al lago',
     location: 'Malibú, California',
-    rating: '4.9',
-    description: 'Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.',
+    rating: '5.0',
+    description: 'Si buscas un lugar tranquilo y relajante para desconectar de la rutina diaria, ¡has encontrado el lugar perfecto! Nuestra casa de campo es ideal para parejas o familias pequeñas que buscan disfrutar de la naturaleza y la serenidad que ofrece el lago.',
     images: [
-      require('../../assets/images/cabin_lake.webp'),
-      require('../../assets/images/cabin_lake.webp'),
-      require('../../assets/images/cabin_lake.webp'),
-      require('../../assets/images/cabin_lake.webp')
+      require('../../assets/images/cabin_lake_1.webp'),
+      require('../../assets/images/cabin_lake_2.webp'),
+      require('../../assets/images/cabin_lake_3.webp')
     ],
-    price: '$400/noche'
+    price: '$400/noche',
+    locationCoordinates: {
+      latitude: -24.1893858,
+      longitude: -65.2938462
+    }
   },
   {
     id: '5',
     title: 'Cabaña aislada en el bosque',
     location: 'Malibú, California',
-    rating: '4.9',
-    description: 'Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.',
+    rating: '3.8',
+    description: 'Descubre un lugar mágico y encantador en nuestra cabaña en el bosque. Rodeado de árboles y aire fresco, nuestro alojamiento es perfecto para aquellas personas que quieren alejarse de la ciudad y disfrutar de la tranquilidad que ofrece la naturaleza. La cabaña cuenta con una acogedora sala de estar con chimenea, una cocina totalmente equipada, y un dormitorio cómodo y espacioso. Además, en el exterior podrás disfrutar de una amplia terraza con vistas espectaculares del bosque y una parrilla para hacer barbacoas. ¡Reserva ahora y déjate seducir por la belleza de la naturaleza!',
     images: [
-      require('../../assets/images/cabin_woods.webp'),
-      require('../../assets/images/cabin_woods.webp'),
-      require('../../assets/images/cabin_woods.webp'),
-      require('../../assets/images/cabin_woods.webp')
+      require('../../assets/images/cabin_woods_1.webp'),
+      require('../../assets/images/cabin_woods_2.webp'),
+      require('../../assets/images/cabin_woods_3.webp')
     ],
-    price: '$300/noche'
+    price: '$300/noche',
+    locationCoordinates: {
+      latitude: -24.1893858,
+      longitude: -65.2938462
+    }
   },
   {
     id: '6',
     title: 'Loft moderno en la ciudad',
     location: 'Malibú, California',
-    rating: '4.9',
-    description: 'Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.',
+    rating: '2.6',
+    description: 'Disfruta de una experiencia única en nuestro moderno loft. Con un diseño elegante y sofisticado, nuestro alojamiento es ideal para aquellos que buscan un lugar cómodo y céntrico en la ciudad.',
     images: [
-      require('../../assets/images/loft_modern.webp'),
-      require('../../assets/images/loft_modern.webp'),
-      require('../../assets/images/loft_modern.webp')
+      require('../../assets/images/loft_modern_1.webp'),
+      require('../../assets/images/loft_modern_2.webp'),
+      require('../../assets/images/loft_modern_3.webp')
     ],
-    price: '$350/noche'
+    price: '$350/noche',
+    locationCoordinates: {
+      latitude: -24.1893858,
+      longitude: -65.2938462
+    }
   },
   {
     id: '7',
     title: 'Casa histórica',
     location: 'Malibú, California',
-    rating: '4.9',
-    description: 'Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.',
+    rating: '4.6',
+    description: '¡Sumérgete en la historia en nuestra casa histórica! Con más de 100 años de antigüedad, nuestra casa ha sido restaurada para ofrecer una experiencia auténtica y única a nuestros huéspedes. Disfruta del encanto y la elegancia del pasado en tu próxima estancia.',
     images: [
-      require('../../assets/images/house_historical.webp'),
-      require('../../assets/images/house_historical.webp'),
-      require('../../assets/images/house_historical.webp'),
-      require('../../assets/images/house_historical.webp')
+      require('../../assets/images/house_historical_1.webp'),
+      require('../../assets/images/house_historical_2.webp'),
+      require('../../assets/images/house_historical_3.webp')
     ],
-    price: '$450/noche'
+    price: '$450/noche',
+    locationCoordinates: {
+      latitude: -24.1893858,
+      longitude: -65.2938462
+    }
   }
 ]
